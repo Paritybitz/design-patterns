de7e85bcb0cb43436ba2264103289e5cb69c27f4
Ali Mohsin PC <ali.mohsin05@gmail.com>
2025-04-13 09:26:47 -0400

    find file

--- design-patterns/src/DesignPatterns/Singleton/LoggerSingleton.java
+++ design-patterns/src/DesignPatterns/Singleton/LoggerSingleton.java
@@ -1,9 +1,14 @@
 package DesignPatterns.Singleton;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Logger;
 
 public class LoggerSingleton {
     public static LoggerSingleton INSTANCE;
+    private File file;
 
     private LoggerSingleton() {
     }
@@ -15,22 +20,40 @@ public class LoggerSingleton {
         return INSTANCE;
     }
 
-    public void signInUser(String name, FileSingleton fileSingleton) {
-        try{
-            File file = fileSingleton.getFile();
-            
-            
+    public File createFile(String fileName) {
+        if (file == null) {
+            try {
+                File file = new File(fileName);
+                if (file.createNewFile())
+                    System.out.println("File created: " + file.getName());
+                else
+                    System.out.println("File already exists");
 
+            } catch (Exception e) {
+                System.out.println("Unable to create file");
+            }
+        }
+        return file;
+    }
 
-
-        } catch (Exception e) {
-            System.out.println("User signin failed");
+    public void signInUser(String name) {
+        if (file == null) {
+            System.out.println("Create file first");
+            return;
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+            writer.write(name);
+            writer.newLine();
+        } catch (IOException e) {
+            System.err.println("An error occurred while writing to the file: " + e.getMessage());
         }
 
     }
 
     public static void main(String[] args) {
-        FileSingleton fileSingleton = FileSingleton.getInstance();
-        fileSingleton.createFile("log.txt");
+        LoggerSingleton logger = LoggerSingleton.getInstance();
+        logger.createFile("log.txt");
+        logger.signInUser("Barak Obama");
+        logger.signInUser("Donald Trump");
     }
 }
